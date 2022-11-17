885ab0b850bd5262628a85e97297375478d34901
kristoffer.olsson <kristoffer.olsson@DATOR-13>
2022-11-17 12:33:06 +0100

    Add project files.

---
+++ ArticleOpenUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleOpenUI.ViewModels
+{
+    class ShellViewModel
+    {
+		private string m_Name = "Person";
+
+		public string Name
+		{
+			get { return m_Name; }
+			set { m_Name = value; }
+		}
+
+	}
+}
