bdb8a63fd95077f7afdc2491b43a90b95973e20a
Bark <kristoffer@nosslo.nu>
2022-12-15 15:11:55 +0100

    Abstract enum ArticleType into its own class

---
+++ ArticleOpenUI/Models/ArticleType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleOpenUI.Models
+{
+    internal enum ArticleType
+    {
+		None,
+		Tool,
+		Modification,
+		Plastic,
+		PlasticVariant
+    }
+}
