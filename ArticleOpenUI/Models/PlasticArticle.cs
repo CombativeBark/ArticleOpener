fa53abe1b1f677fae0ce1bf8d92d267e5f4980d8
bark <kristoffer@nosslo.nu>
2022-12-16 20:11:09 +0100

    Extend Article class to more specific article types

---
+++ ArticleOpenUI/Models/PlasticArticle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ArticleOpenUI.Models
+{
+    public class PlasticArticle : ArticleBase
+    {
+        private bool _isVariant = false;
+
+        public override ArticleType Type => ArticleType.Tool;
+        public override string Url => $@"http://server1:85/tool/{Name}";
+        public override string Path
+        { 
+            get 
+            { 
+                if (_isVariant)
+                    return $@"\\server1\ArticleFiles\ArtikelFiler\{Name.Substring(0, 7)}";
+                else 
+                    return $@"\\server1\ArticleFiles\ArtikelFiler\{Name}\{Name}";
+            }
+        }
+
+
+        public PlasticArticle(string name)
+        {
+            Name = name;
+
+            _isVariant = IsVariant(Name);
+        }
+
+        public override List<PlasticArticle> GetChildren()
+        {
+            return new List<PlasticArticle>{ };
+        }
+
+        private bool IsVariant(string name)
+        {
+            const string regex = @"^\d{6}P-\d$";
+
+            if (Regex.IsMatch(name, regex, RegexOptions.Compiled))
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ ArticleOpenUI/Models/ToolArticle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ArticleOpenUI.Models
+{
+    public class ToolArticle : ArticleBase
+    {
+        private bool _isModification = false;
+        private List<PlasticArticle> _children;
+
+        public override ArticleType Type => ArticleType.Tool;
+        public override string Url => $@"http://server1:85/tool/{Name}";
+        public override string Path
+        { 
+            get 
+            { 
+                if (_isModification)
+                    return $@"\\server1\ArticleFiles\ArtikelFiler\{Name.Substring(0, 7)}";
+                else 
+                    return $@"\\server1\ArticleFiles\ArtikelFiler\{Name}\{Name}";
+            }
+        }
+
+        public ToolArticle(string name)
+        {
+            Name = name;
+            _isModification = IsModification(Name);
+
+            _children = GetChildren();
+
+        }
+
+        public override List<PlasticArticle> GetChildren()
+        {
+            List<PlasticArticle> result = new();
+
+            HtmlDocument htmlDoc = new HtmlDocument();
+            HtmlWeb htmlWeb = new HtmlWeb();
+            htmlDoc = htmlWeb.Load(Url);
+
+            throw new NotImplementedException();
+            return result;
+        }
+
+        public bool IsModification(string name)
+        {
+            const string regex = @"^\d{6}V[1-9]$";
+
+            if (Regex.IsMatch(name, regex, RegexOptions.Compiled))
+                return true;
+
+            return false;
+        }
+    }
+}
